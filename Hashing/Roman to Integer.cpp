d28ef4a04fc31d0a98ca3ef365834059fd9f79b9
nani praneeth <pchowdary889@gmail.com>
2023-06-09 15:39:50 +0530

    organising folders

---
+++ Hashing/Roman to Integer.cpp
@@ -0,0 +1,29 @@
+// link https://leetcode.com/problems/roman-to-integer/description/?envType=featured-list&envId=top-interview-questions
+
+class Solution
+{
+public:
+    int romanToInt(string s)
+    {
+        map<char, int> rtoi({
+            {'I', 1},
+            {'V', 5},
+            {'X', 10},
+            {'L', 50},
+            {'C', 100},
+            {'D', 500},
+            {'M', 1000},
+        });
+        int a = 0;
+        for (int i = 0; i < s.size(); i++)
+        {
+            if (i != s.size() - 1 && rtoi[s[i]] < rtoi[s[i + 1]])
+            {
+                a -= rtoi[s[i]];
+                continue;
+            }
+            a += rtoi[s[i]];
+        }
+        return a;
+    }
+};
