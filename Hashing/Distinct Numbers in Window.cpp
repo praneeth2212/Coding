69eb3d11a95065ebe3994d2e1f1b58fc9385e816
nani praneeth <pchowdary889@gmail.com>
2023-06-17 23:13:35 +0530

    organising folders

---
+++ Hashing/Distinct Numbers in Window.cpp
@@ -0,0 +1,40 @@
+// link https://www.interviewbit.com/problems/distinct-numbers-in-window/
+
+vector<int> Solution::dNums(vector<int> &A, int B)
+{
+
+    unordered_map<int, int> mp;
+
+    vector<int> ans;
+
+    int j = 0;
+
+    int i = 0;
+
+    while (i != B)
+    {
+
+        mp[A[i]]++;
+
+        i++;
+    }
+
+    ans.push_back(mp.size());
+
+    for (int i = B; i < A.size(); i++)
+    {
+
+        mp[A[i]]++;
+
+        mp[A[j]]--;
+
+        if (mp[A[j]] == 0)
+            mp.erase(A[j]);
+
+        j++;
+
+        ans.push_back(mp.size());
+    }
+
+    return ans;
+}
