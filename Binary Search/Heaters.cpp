45b603308f3dbbfb93b424eadf7a1140257ba43a
nani praneeth <pchowdary889@gmail.com>
2023-06-19 17:53:36 +0530

    organising folders

---
+++ Binary Search/Heaters.cpp
@@ -0,0 +1,21 @@
+// link https://leetcode.com/problems/heaters/description/
+
+class Solution
+{
+public:
+    int findRadius(vector<int> &houses, vector<int> &heaters)
+    {
+        sort(houses.begin(), houses.end());
+        sort(heaters.begin(), heaters.end());
+        int ans = 0;
+        int i = 0; // Point to the heater that currently used
+        for (const int house : houses)
+        {
+            while (i + 1 < heaters.size() &&
+                   house - heaters[i] > heaters[i + 1] - house)
+                ++i; // Next heater is better
+            ans = max(ans, abs(heaters[i] - house));
+        }
+        return ans;
+    }
+};
